08092445e1d8a23fdb1776a2fb96546c9e8e441b
Alexandru Pana <astronothing@gmail.com>
2017-03-13 20:39:11 +0200

    Add actual content

--- script.js
+++
@@ -1,9 +0,0 @@
-var textArea = document.getElementsByClassName("floating-text")[0]
-
-var createText = function (classname) {
-    var text = document.createElement("div");
-    text.className = "text " + classname;
-    return text;
-};
-
-textArea.appendChild(createText("foreground middle"));
---
+++ static/gallery/gallery.js
@@ -0,0 +1,56 @@
+Gallery = {
+    "images": [
+        {
+            "thumbnail": "static/gallery/01_thumb.png",
+            "src": "static/gallery/01_full.png",
+            "w": 437,
+            "h": 437,
+            "title": "Bucătărie",
+            "description": "Lemn masiv"
+        }, {
+            "thumbnail": "static/gallery/02_thumb.png",
+            "src": "static/gallery/02_full.png",
+            "w": 657,
+            "h": 329,
+            "title": "Bucătărie",
+            "description": "MDF"
+        }, {
+            "thumbnail": "static/gallery/03_thumb.png",
+            "src": "static/gallery/03_full.png",
+            "w": 2000,
+            "h": 1000,
+            "title": "Masă cu scaune"
+        }, {
+            "thumbnail": "static/gallery/04_thumb.png",
+            "src": "static/gallery/04_full.png",
+            "w": 1000,
+            "h": 1000,
+            "title": "Bucătărie"
+        }, {
+            "thumbnail": "static/gallery/05_thumb.png",
+            "src": "static/gallery/05_full.png",
+            "w": 583,
+            "h": 292,
+            "title": "Bucătărie"
+        }, {
+            "thumbnail": "static/gallery/06_thumb.png",
+            "src": "static/gallery/06_full.png",
+            "w": 1000,
+            "h": 1000,
+            "title": "Masă"
+        }, {
+            "thumbnail": "static/gallery/07_thumb.png",
+            "src": "static/gallery/07_full.png",
+            "w": 636,
+            "h": 1272,
+            "title": "Scări"
+        }, {
+            "thumbnail": "static/gallery/09_thumb.png",
+            "src": "static/gallery/09_full.png",
+            "w": 1000,
+            "h": 1000,
+            "title": "Mobilă sufragerie",
+            "description": "Detalii antichizate"
+        }
+    ]
+};
